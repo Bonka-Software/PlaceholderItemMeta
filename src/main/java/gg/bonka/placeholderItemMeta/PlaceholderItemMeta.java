776cd7e409ff02793f21ec64b0f9b92b3b988fa2
Stanley-Dam <42673142+Stanley-Dam@users.noreply.github.com>
2025-04-06 16:33:32 +0200

    Initial commit

---
+++ src/main/java/gg/bonka/placeholderItemMeta/PlaceholderItemMeta.java
@@ -0,0 +1,17 @@
+package gg.bonka.placeholderItemMeta;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlaceholderItemMeta extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
